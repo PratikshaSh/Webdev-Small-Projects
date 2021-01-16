c9aef6ac3077403810835b7ebf37228ee340d006
PratikshaSh <pratiksha8520@gmail.com>
2021-01-17 01:00:25 +0530

    add deleting todos functionality

--- TodoListProject/assets/js/todos.js
+++ TodoListProject/assets/js/todos.js
@@ -3,3 +3,10 @@ $("li").click(function(){
     $(this).toggleClass("completed");
  
 });
+//click on X to delete todos
+$("span").click(function(event) {
+   $(this).parent().fadeOut(500,function() {
+       $(this).remove();
+   });
+   event.stopPropagation();//jquery method that will stop the event from bubbling up 
+});
